ed0711ed0311869f82750c21fbe9449c637c552f
Shashank Singh <singhss@usc.edu>
2015-02-23 18:10:56 +0530

    adding files to postagger

---
+++ postagging/perceplearn.py
@@ -0,0 +1,67 @@
+import json
+import sys
+import random
+files=sys.argv
+
+def updateweights(context,actualclass):
+        predictedclass={}
+        for i in range(len(classlist)):
+            predictedclass[classlist[i]]=0
+        for i in range(len(classlist)):
+            for word in context:
+                try:
+                    predictedclass[classlist[i]]+=feature[word]*weight[classlist[i]][word]
+                except:
+                    predictedclass[classlist[i]]+=0
+        argmax=classlist[0]
+        for key in predictedclass:
+            if predictedclass[key]>predictedclass[argmax]:
+                argmax=key
+        if argmax!=actualclass:
+            for word in context:
+                weight[argmax][word]-=feature[word]
+                weight[actualclass][word]+=feature[word]
+    
+def train(classlist_passed,lines):
+    N=10
+    global weight,weightavg
+    global classlist
+    classlist=classlist_passed
+    weight={}
+    weightavg={}
+    for eachclass in classlist:
+        weight[eachclass]={}
+        weightavg[eachclass]={}
+    for a in range(N):
+        for line in random.sample(lines,len(lines)):
+            words=line.split()
+            for index in range(2,len(words)-1,2):
+                global feature
+                feature={}
+                wprev='w_prev'+words[index-2]
+                wcurr='w_curr'+words[index]
+                try:
+                    wnext='w_next'+words[index+2]
+                except:
+                    wnext='w_next'+words[index+1]
+                context=[wprev,wcurr,wnext]
+                for eachword in context:
+                    feature[eachword]=1
+                for eachclass in classlist:
+                    for eachword in context:
+                        if eachword not in weight[eachclass]:
+                            weight[eachclass][eachword]=0
+                updateweights(context,words[index-1])
+        for eachclass in weight:
+            for word in weight[eachclass]:
+                try:
+                    weightavg[eachclass][word]+=weight[eachclass][word]
+                except:
+                    weightavg[eachclass][word]=weight[eachclass][word]
+        
+        f1=open(str(files[2]),"w+")
+        encode=json.dumps(weightavg)
+        f1.write(encode)
+        f1.close()          
+                    
+    
