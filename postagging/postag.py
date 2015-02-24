ecbd21a26f06cb9c1ee5bf4569d0b61b7757b61e
Shashank Singh <singhss@usc.edu>
2015-02-24 20:02:05 +0530

    modified files

--- postagging/postag.py
+++ postagging/postag.py
@@ -1,27 +1,55 @@
 import sys
 import json
-from percepclassify import percepclassify
 files=sys.argv
+sys.stdin = codecs.getreader('latin-1')
 
-    
+
 def loadmodell():
-    f=open(str(files[1]),'r')
+    f=open(str(files[1]),'r',encoding='latin-1')
     weightavg=json.loads(f.read())
     return weightavg
 
 def classify(classlist):
-    lines=sys.stdin.readline()
-    output=''
-    wordss=lines.split()
-    line='%%%BEGIN%%%'+' '
-    for word in wordss:
-        line+=word+' '
-    line+='%%%END%%%'
-    percepclassify(line,classlist,weightavg)
-            
+    for eachline in sys.stdin.readlines():
+        wordss=eachline.split()
+        line='%%%BEGIN%%%'+' '
+        for word in wordss:
+            line+=word.rsplit('/',1)[0]+' '
+        line+='%%%END%%%'
+        percepclassify(line,classlist,weightavg)
 
+def percepclassify(line,classlist,weightavg):
+    output=''
+    words=line.split()
+    for index in range(1,len(words)-1):
+        wprev='w_prev'+words[index-1]
+        wcurr='w_curr'+words[index]
+        wnext='w_next'+words[index+1]
+        classifydev={}
+        feature={}
+        context=[wprev,wcurr,wnext]
+        for eachword in context:
+            feature[eachword]=1
+        for eachclass in classlist:
+            classifydev[eachclass]=0
+        for eachclass in classlist:
+            for eachword in context:
+                try:
+                    classifydev[eachclass]+=feature[eachword]*weightavg[eachclass][eachword]
+                except:
+                    classifydev[eachclass]+=0
+        maximum=classlist[0]
+        for eachclass in classlist:
+            if classifydev[eachclass]>classifydev[maximum]:
+                maximum=eachclass
+        output+=words[index]+'/'+maximum+' '
+    print(output)
+    
+    
 weightavg=loadmodell()
 classlist=[]
 for key in weightavg:
     classlist.append(key)
 classify(classlist)
+    
+
